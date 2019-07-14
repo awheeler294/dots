001db81de3e43518adaeafe3b905b30b324b7e82
awheeler294 <awheeler294@gmail.com>
2019-07-14 10:18:27 -0600

    Polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/sh
+#echo "kaunchibng Polybar" 1>&2
 # Terminate already running bar instances
 killall -q polybar
 
@@ -8,4 +9,11 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch bar
 polybar future &
 
+echo "$(polybar --list-monitors | cut -d":" -f1)"
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload status &
+    echo "Launched bar 'status' on monitor $m" 1>&2
+done
+
 echo "Bar launched"
+#echo "Bar launched" 1>&2
