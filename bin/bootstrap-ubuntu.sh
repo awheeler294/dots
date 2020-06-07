bdc3496a8a09d4e719ae8798d2c7c096d56a3599
awheeler294 <awheeler294@gmail.com>
2020-06-07 18:05:40 +0000

    make sure all dependencies installed

--- bin/bootstrap-ubuntu.sh
+++ bin/bootstrap-ubuntu.sh
@@ -3,13 +3,16 @@
 git clone https://github.com/zsh-users/zsh-history-substring-search ${HOME}/.config/zsh-plugins/zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-autosuggestions          ${HOME}/.config/zsh-plugins/zsh-autosuggestions
 
-if [[ $(lsb_release -rs) == "16.04" ]]; then 
-   sudo add-apt-repository ppa:neovim-ppa/stable
-fi
-
 sudo apt update
-sudo apt install zsh zsh-syntax-highlighting grc neovim nodejs tree
-pip install --user jedi
+sudo apt upgrade
+
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt update
+sudo apt upgrade
+
+sudo apt install zsh zsh-syntax-highlighting grc neovim nodejs npm tree python3-pip
+pip3 install --user jedi
 
 wget https://raw.githubusercontent.com/thestinger/termite/master/termite.terminfo -P /tmp/
 tic -x /tmp/termite.terminfo
